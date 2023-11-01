7e336ca02554e2671e7c81953015a2fd489725a6
sarahc-dev <sarahclements@hotmail.co.uk>
2023-11-01 00:27:59 +0000

    initial files

---
+++ src/main/java/com/projects/invoiceapp/InvoiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.projects.invoiceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/invoiceapp/InvoiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.invoiceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
